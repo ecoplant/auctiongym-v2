ea65e8b1677a74b856bfbbe83879f03605cd3990
seunghoonkim <extraterrestrialpi@gmail.com>
2023-05-20 22:19:29 +0900

    commit

--- src/Allocator.py
+++ src/Allocator.py
@@ -58,3 +58,68 @@ class LogisticAllocator(Allocator):
 
     def get_uncertainty(self):
         return self.model.get_uncertainty()
+
+class NeuralAllocator(Allocator):
+    def __init__(self, rng, item_features, lr, batch_size, weight_decay, latent_dim, num_epochs, context_dim, mode, eps=None, prior_var=None):
+        super().__init__(rng, item_features)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.mode = mode
+        self.num_epochs = num_epochs
+        self.context_dim = context_dim
+        if self.mode=='Epsilon-greedy':
+            self.net = NeuralRegression(context_dim+self.feature_dim, latent_dim).to(self.device)
+            self.eps = eps
+        else:
+            raise NotImplementedError
+        self.count = 0
+        self.lr = lr
+        self.batch_size = batch_size
+        self.weight_decay = weight_decay
+        self.optimizer = torch.optim.Adam(self.net.parameters(), lr=self.lr, weight_decay=self.weight_decay, amsgrad=True)
+
+    def update(self, contexts, items, outcomes):
+        self.count += 1
+
+        X = np.concatenate([contexts, self.item_features[items]], axis=1)
+        X, y = torch.Tensor(X).to(self.device), torch.Tensor(outcomes).to(self.device)
+        N = X.shape[0]
+        if N<10:
+            return
+
+        self.net.train()
+        batch_size = min(N, self.batch_size)
+
+        for epoch in range(int(self.num_epochs)):
+            shuffled_ind = self.rng.choice(N, size=N, replace=False)
+            epoch_loss = 0
+            for i in range(int(N/batch_size)):
+                self.optimizer.zero_grad()
+                ind = shuffled_ind[i*batch_size:(i+1)*batch_size]
+                X_ = X[ind]
+                y_ = y[ind]
+                if self.mode=='Epsilon-greedy':
+                    loss = self.net.loss(self.net(X_).squeeze(), y_)
+                else:
+                    loss = self.net.loss(self.net(X_).squeeze(), y_, N)
+                loss.backward()
+                self.optimizer.step()
+                epoch_loss += loss.item()
+        self.net.eval()
+
+    def estimate_CTR(self, context, TS=False):
+        if TS:
+            X = torch.Tensor(np.concatenate([np.tile(context.reshape(1,-1),(self.K, 1)), self.item_features],axis=1)).to(self.device)
+            y = []
+            for i in range(self.num_samples):
+                y.append(self.net(X, MAP=False).numpy(force=True).reshape(-1))
+            y = np.stack(y)
+            std = np.std(y, axis=0)
+            self.uncertainty.append(np.mean(std))
+            return y[0], std
+        else:
+            X = torch.Tensor(np.concatenate([np.tile(context.reshape(1,-1),(self.K, 1)), self.item_features],axis=1)).to(self.device)
+            if self.mode=='TS':
+                return self.net(X, MAP=True).numpy(force=True).reshape(-1)
+            else:
+                return self.net(X).numpy(force=True).reshape(-1)
+
